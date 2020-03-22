b8c56c3934bbdb215711fdc86291c3f49eb0aca5
duiprogramming <62176479+duiprogramming@users.noreply.github.com>
2020-03-22 16:16:59 +0530

    NodeMCU LED ON OFF coding

---
+++ NodeMCU_LED_ONOFF.ino
@@ -0,0 +1,11 @@
+void setup() {
+  pinMode(13, OUTPUT);  //Pin 13 initialized as output
+}
+
+void loop() {
+  digitalWrite(13, HIGH); //LED turn on setting valtage as HIGH
+  delay(1500);             //Wait half second
+  digitalWrite(13, LOW);  //LED turn off setting voltage as LOW
+  delay(500);             //Wait half second
+  
+}
